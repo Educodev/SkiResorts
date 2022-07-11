52f9b66eab41761123dcc16254bdd14259f69803
Eduardo Herrera <eduardodesarrollador@icloud.com>
2022-07-11 13:41:13 +0200

    Initial Commit

---
+++ SkiResorts/SkiResortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkiResortsApp.swift
+//  SkiResorts
+//
+//  Created by Eduardo Herrera on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SkiResortsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
